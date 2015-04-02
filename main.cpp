188cfd5e3662c17441381d8339e17cf6e8b1b525
Wyatt Childers <wchilders@nearce.com>
2015-04-02 00:20:40 -0400

    Added proper command parsing

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 
 #include <iostream>
+#include <regex>
+#include <string>
 
-#include "Util/FlexGrid.hpp"
 #include "SeamCarver.hpp"
 #include "ImageLoader.hpp"
+#include "Util/FlexGrid.hpp"
 
 int main(int argc, char* argv[]) {
-  ImageLoader loader;
-  loader.loadFile("test.pgm");
-  FlexGrid<int> p = seamCarve(loader.getGrid(), CarvingMode::VERTICAL, 1);
-  FlexGrid<int> f = seamCarve(p, CarvingMode::HORIZONTAL, 5);
-  loader.setGrid(f);
-  loader.exportFile("test-res.pgm");
+  if (argc == 4) {
+    const std::regex e("(\\.pgm)");
+
+    // Trigger a simulation based on arguments
+    std::string file = std::regex_replace(argv[1], e, "");
+    unsigned int vert = std::atoi(argv[2]);
+    unsigned int horiz = std::atoi(argv[3]);
+
+    ImageLoader loader;
+    loader.loadFile(file + ".pgm");
+    FlexGrid<int> p = seamCarve(loader.getGrid(), CarvingMode::VERTICAL, vert);
+    FlexGrid<int> f = seamCarve(p, CarvingMode::HORIZONTAL, horiz);
+    loader.setGrid(f);
+    loader.exportFile(file + "_processed.pgm");
+  } else {
+    throw std::runtime_error("Illegal number of arguments!");
+  }
   return 0;
 }
