a138a2652400cbc06e8aa75eab3b5fcec64c719c
Wyatt Childers <wchilders@nearce.com>
2015-04-08 20:41:10 -0400

    Documented main.cpp

--- main.cpp
+++ main.cpp
@@ -8,18 +8,28 @@
 #include "Util/FlexGrid.hpp"
 
 int main(int argc, char* argv[]) {
+  // Check that the proper amount of arguments have
+  // been supplied
   if (argc == 4) {
-    const std::regex e("(\\.pgm)");
-
-    // Trigger a simulation based on arguments
-    std::string file = std::regex_replace(argv[1], e, "");
+    // Remove the file name from the file extension
+    std::string file = std::regex_replace(argv[1], std::regex("(\\.pgm)"), "");
+    // Get the number of vertical seams to remove
     unsigned int vert = std::atoi(argv[2]);
+    // Get the number of horizontal seams to remove
     unsigned int horiz = std::atoi(argv[3]);
 
+    // Establish an ImageLoader, then load the file, with the removed
+    // ".pgm" extension readded
     ImageLoader loader;
     loader.loadFile(file + ".pgm");
+
+    // Perform the removal of the vertical seams, followed by the
+    // removal of the horizontal seams
     FlexGrid<int> p = seamCarve(loader.getGrid(), CarvingMode::VERTICAL, vert);
     FlexGrid<int> f = seamCarve(p, CarvingMode::HORIZONTAL, horiz);
+
+    // Update the pixel grid stored in the ImageLoader,
+    // and export the file, as being processed
     loader.setGrid(f);
     loader.exportFile(file + "_processed.pgm");
   } else {
